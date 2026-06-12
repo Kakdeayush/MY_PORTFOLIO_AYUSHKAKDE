4a1a93e79cac774d6962e171cd0f83320714f74a
Kakdeayush <kakdeayush376@gmail.com>
2026-06-13 02:03:23 +0530

    change the project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import ecommerceImg from "../assets/ecommerce-web.png";
+import banner2 from "../assets/banner2.png";
 import './Projects.css';
 
 const projectsData = [
+  
     {
         id: 1,
+        title: 'Community Civic Issue Reporting Website ',
+        description: 'Developed a civic issue reporting website using Java, Spring Boot, React.js, and MySQL to streamline complaint registration and resolution tracking.',
+        image:banner2,
+        tags: ['Java', 'Springboot', 'Vite', 'React'],
+        github: 'https://github.com/Kakdeayush/CIVICRESOLVER'
+    },
+    
+    {
+        id: 2,
         title: 'E-Commerce Fraud Detection',
         description: 'A machine learning and Java Spring Boot application that detects fraudulent transactions in real-time, preventing financial losses using random forest algorithms.',
         image:ecommerceImg,
         tags: ['Java', 'Spring Boot', 'MySQL', 'Machine Learning'],
         github: 'https://github.com/Kakdeayush/E-COMMERCE-FRAUD-DETECTION-WEB'
-    },
-    {
-        id: 2,
-        title: 'Cartoon Portfolio Website',
-        description: 'A responsive, animated portfolio website built with React and custom CSS to showcase my projects and skills with a playful awwwards-style aesthetic.',
-        image: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
-        tags: ['React', 'CSS', 'Vite', 'Animations'],
-        github: 'https://github.com'
     },
     {
         id: 3,
